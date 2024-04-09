4446fcccd72edf250c52af7faf5336873e7e88c2
stepanover.tech <gustutest@gmail.com>
2024-04-09 15:33:09 +0500

    Commit

---
+++ netbox/plugins/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+
+setup(
+    name='fast_add_device',
+    version='0.2.1',
+    description='Scan device by ip',
+    long_description='Scan device by ip for add new devices',
+    long_description_content_type='text',
+    author='Steanov Evgeniy',
+    author_email='jacksontur@yandex.ru',
+    license='Nginx',
+    install_requires=[],
+    packages=["fast_add_device"],
+    package_data={"fast_add_device": ["templates/fast_add_device/*.html","device_types/*","nb_exec/*"]},
+    zip_safe=False
+)
+
