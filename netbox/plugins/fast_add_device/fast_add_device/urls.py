ecd95ebedd24200a4e1b62fd37fadf9efac3f3f0
stepanover.tech <gustutest@gmail.com>
2024-05-20 13:21:43 +0200

    Commit

--- netbox/plugins/fast_add_device/fast_add_device/urls.py
+++ netbox/plugins/fast_add_device/fast_add_device/urls.py
@@ -4,12 +4,13 @@
 from django.urls import path
 
 from .views import Add_Device_Active_View,Add_Device_Offline_View,\
-    Change_Device_Active_View ,get_location,get_racks,Add_Device_CSV_Online_View
+    Change_Device_Active_View ,get_location,get_racks,Add_Device_CSV_Online_View,Add_Sites_CSV_View
 
 urlpatterns = [
     path('add_device_active/', Add_Device_Active_View.as_view(), name='add_device_active'),
     path('add_device_offline/', Add_Device_Offline_View.as_view(), name='add_device_offline'),
     path('add_device_csv/',Add_Device_CSV_Online_View.as_view(),name='add_device_csv'),
+    path('add_sites_csv/',Add_Sites_CSV_View.as_view(),name='add_sites_csv'),
     path('edit_device_active/',Change_Device_Active_View.as_view(),name='edit_device_active'),
     path('get_location/', get_location, name='get_location'),
     path('get_racks/', get_racks, name='get_racks'),
